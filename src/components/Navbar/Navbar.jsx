d703632ddfe2df84b7b32cf901e8e67bc3e8dba3
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-03 20:16:22 +0000

    working on the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div>
+      <img src={images}/>
+    </div>
+  </nav>
 );
 
 export default Navbar;

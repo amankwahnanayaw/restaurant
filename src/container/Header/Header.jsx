3d50d21fff54161d788e22dffed30a646dcc4d27
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-04 23:53:07 +0000

    working on the heading

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,8 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title='Chase The New Flavour'/>
       <h1 className='app__header-hi'>The Key to Fine Dinning</h1>
-      <p className='p__opensans' style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie.
-      Condimentum volutpat morbi facilisis quam scelerisque
-      sapien. Et, penatibus aliquam amet tellus</p>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>Let it be the land, but let us live in old age.
+      Seasoning of the food brings out the taste and satisfaction, in the end, there is a lot of love to pass around</p>
     </div>
 
     <div className='app_wrapper_img'>

b877b0b0991c15b2a6cc6b22dda90303c3bb7894
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-04 23:54:42 +0000

    working on the heading

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => (
       <h1 className='app__header-hi'>The Key to Fine Dinning</h1>
       <p className='p__opensans' style={{ margin: '2rem 0' }}>Let it be the land, but let us live in old age.
       Seasoning of the food brings out the taste and satisfaction, in the end, there is a lot of love to pass around</p>
+      <button type='button' className='app__header'></button>
     </div>
 
     <div className='app_wrapper_img'>

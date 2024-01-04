67732e34bc04f7521bb96ca67d996717a3603166
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-04 23:48:33 +0000

    working on the heading

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,9 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title='Chase The New Flavour'/>
       <h1 className='app__header-hi'>The Key to Fine Dinning</h1>
-      <p className='p__opensans' style={{ margin: '2rem 0' }}></p>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie.
+      Condimentum volutpat morbi facilisis quam scelerisque
+      sapien. Et, penatibus aliquam amet tellus</p>
     </div>
 
     <div className='app_wrapper_img'>

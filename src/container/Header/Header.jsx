c77c81667ae1092b787e8c08cb3902875d7a61c1
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-05 07:57:31 +0000

    working on the about us

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
       <SubHeading title='Chase The New Flavour'/>
       <h1 className='app__header-h1'>The Key to Fine Dinning</h1>
       <p className='p__opensans' style={{ margin: '2rem 0' }}>Let it be the land, but let us live in old age.
-      Seasoning of the food brings out the taste and satisfaction, in the end, there is a lot of love to pass around</p>
+      Seasoning of the food brings out the taste and satisfaction, in the end, there is a lot of love to be passed around</p>
       <button type='button' className='custom__button'>Explore Menu</button>
     </div>
 

a8dd028af936bfc65dba91bd405f5d949b2ed89a
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-05 08:32:40 +0000

    working on the about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,6 +8,16 @@ const AboutUs = () => (
     <div className='app__aboutus-overlay flex__center'>
       <img src={ images.G } alt='g letter' />
     </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img' />
+        <p className='p__opensans'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur distinctio voluptatum soluta odit accusantium minima voluptas odio fuga molestiae velit nesciunt neque dolorem repellat corrupti, eum commodi pariatur saepe sed.</p>
+        <button type='button' className='custom__button'>Know More</button>
+
+      </div>
+    </div>
   </div>
 );
 

a6b67c981419769fa8b9d085f4fdd3668f3b4a5b
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-05 08:37:35 +0000

    working on the about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -15,8 +15,19 @@ const AboutUs = () => (
         <img src={images.spoon} alt="about_spoon" className='spoon__img' />
         <p className='p__opensans'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur distinctio voluptatum soluta odit accusantium minima voluptas odio fuga molestiae velit nesciunt neque dolorem repellat corrupti, eum commodi pariatur saepe sed.</p>
         <button type='button' className='custom__button'>Know More</button>
-
       </div>
+
+      <div className='app__about-content_knife flex__center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className='app__aboutus-content flex__center'>
+        <div className='app__aboutus-content_history'>
+          <h1 className='headtext__cormorant'>Our History</h1>
+          <img src={images.spoon} alt="about_spoon" className='spoon__img' />
+          <p className='p__opensans'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur distinctio voluptatum soluta odit accusantium minima voluptas odio fuga molestiae velit nesciunt neque dolorem repellat corrupti, eum commodi pariatur saepe sed.</p>
+          <button type='button' className='custom__button'>Know More</button>
+        </div>
     </div>
   </div>
 );

a6b43ff2681ad158081d60c1787f34aa9bffa830
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-09 10:47:49 +0000

    working on intro video

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import images from './images';
 import meal from '../assets/meal.mp4';
 import data from './data';
+import vineintro from '../assets/vineintro.mp4'
 
-export { images, meal, data };
+export { images, meal, data, vineintro };
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { BsFillPlayFill, BsPauseFill } from 'react-icons';
 import './Intro.css';
 
 const Intro = () => (

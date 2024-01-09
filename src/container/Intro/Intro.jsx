7382d11d33c05b5c2ed3e2d997d9a4d00daa16ff
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-09 11:04:56 +0000

    working on intro video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-
 import { BsFillPlayFill, BsPauseFill } from 'react-icons';
+
+import { vineintro } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playvideo, setPlayvideo] = React.useState(false);
+  const vidRef = React.useRef()
+
+  return (
+    <div className='app__video'>
+      <video src={vineintro} ref={vidRef} type="video/mp4" loop controls={false} muted />
+      <div className='app__video-overlay flex__center'>
+        
+      </div>
+    </div>
+  );
+}
 
 export default Intro;

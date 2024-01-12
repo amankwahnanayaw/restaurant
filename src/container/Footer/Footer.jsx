b76e76a12022947456afe5655c9663c8292760e3
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-12 08:15:03 +0000

    woking on footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Footeroverlay, Newsletter } from '../../components'
-import {  }
+import { Footeroverlay, Newsletter } from '../../components';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 import './Footer.css';
 
 const Footer = () => (

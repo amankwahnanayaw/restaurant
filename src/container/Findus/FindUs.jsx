295c3acc998e6d239c545dd3ebdd7de79ff13a1c
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-12 07:52:31 +0000

    working on find us

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Contact"/>
+      <h1 className='headtext__cormorant' style={{ marginBottom: '3rem' }}></h1>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 

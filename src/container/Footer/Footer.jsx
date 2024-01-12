1f5558f82b2a58490cd0621485a95b0fbdd6caa0
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-12 21:19:38 +0000

    woking on footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
-
+import { images } from '../../constants';
 import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
@@ -11,10 +11,14 @@ const Footer = () => (
 
     <div className='app__footer-links'>
       <div className='app__footer-links_contact'>
-
+        <h1 className='app__footer-headtext'>Contact Us</h1>
+        <p className='p__opensans'>Fourth Norla Street, Labone</p>
+        <p className='p__opensans'>+233 55 845 0253</p>
+        <p className='p__opensans'>+233 55 845 0253</p>
       </div>
       <div className='app__footer-links_logo'>
-
+        <img src={images.gericht} alt="footer_logo" />
+        <p></p>
       </div>
       <div className='app__footer-links_work'>
 

fb09907b0ade43523e79881df68bce1e9afad4ef
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-13 23:18:11 +0000

    woking on footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -9,7 +9,6 @@ const Footer = () => (
   <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
-
     <div className='app__footer-links'>
       <div className='app__footer-links_contact'>
         <h1 className='app__footer-headtext'>Contact Us</h1>

ba18351f1c158ddce5f1c52e5b5c5a81e396dd50
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-13 22:39:33 +0000

    woking on footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -4,13 +4,13 @@ import { SubHeading } from '../../components';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div className='app__newsLetter'>
+  <div className='app__newsletter'>
     <div app__newsLetter-heading>
       <SubHeading title='NewsLetter'/>
       <h1 className='headtext__cormorant'>Subscribe to Our NewsLetter</h1>
       <p className='p__opensans'>And never miss Latest Updates</p>
     </div>
-    <div className='app__newsLetter-input flex__center'>
+    <div className='app__newsletter-input flex__center'>
       <input type='email' placeholder='Enter your e-mail address' />
       <button className='custom__button'>Subscribe</button>
     </div>
